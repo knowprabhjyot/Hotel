0a40a9b292764d36cfc3da01f2490fb4476ebcbf
Prabhjyot <knowprabhjyot@gmail.com>
2021-03-08 19:25:42 +0530

    added spinner in manage users

--- src/components/home/manage-users/manage-users.js
+++ src/components/home/manage-users/manage-users.js
@@ -1,9 +1,10 @@
-import { Box, Button, makeStyles } from '@material-ui/core';
+import { Box, Button, CircularProgress, makeStyles, Paper, Snackbar } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CreateUserComponent from './create-user/create-user';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const columns = [
   { field: 'id', headerName: 'id', width: 400 },
@@ -21,6 +22,10 @@ export default function ManageUsersComponent() {
 
   const [usersList, setUsersList] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [open, setOpen] = React.useState(false);
+  const [message, setMessage] = React.useState('');
+  const [severity, setSeverity] = React.useState('success');
 
   const classes = useStyles();
   useEffect(() => {
@@ -29,6 +34,7 @@ export default function ManageUsersComponent() {
   }, []);
 
   const getUsersList = async () => {
+    setLoading(true);
     try {
       const response = await axios.get('http://localhost:5000/users');
       if (response) {
@@ -36,10 +42,12 @@ export default function ManageUsersComponent() {
           return { id: _id, email, role }
         })
         setUsersList(newUsersList);
+        setLoading(false);
         return response;
       }
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   }
 
@@ -67,10 +75,29 @@ export default function ManageUsersComponent() {
     setSelectedUser(e.data.id);
   }
 
+  const Alert = ((props) => {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  });
+
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <Box display="flex" width="100%" justifyContent="center">
-      <Box display="flex" flexDirection="column" height="400" width="80%">
-        <Box display="flex" justifyContent="space-between">
+      <Snackbar onClose={handleClose} open={open} autoHideDuration={2000} >
+        <Alert onClose={handleClose} severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
+      { loading ? <CircularProgress color="secondary" /> : null}
+      { !loading ? <Box display="flex" flexDirection="column" height="400" width="80%">
+        <Box display="flex" marginBottom="8px" justifyContent="space-between">
           <CreateUserComponent />
           {selectedUser ? <Button
             variant="contained"
@@ -82,8 +109,10 @@ export default function ManageUsersComponent() {
             Delete
       </Button> : null}
         </Box>
-        <DataGrid autoHeight onRowSelected={selectUserList} rows={usersList} columns={columns} pageSize={5} />
-      </Box>
+        <Paper>
+          <DataGrid autoHeight onRowSelected={selectUserList} rows={usersList} columns={columns} pageSize={5} />
+        </Paper>
+      </Box> : null}
     </Box>
 
   );
